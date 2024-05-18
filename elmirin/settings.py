d8741b333bd8c12ac66d8d1bc1b5498a66912632
elmir ibrahim <elmirmansimov@gmail.com>
2024-05-18 22:18:05 +0400

    Django yapılandırmasını güncelle ve index.html dosyasını taşı

--- elmirin/settings.py
+++ elmirin/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'elmirin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'home', 'templates')],  # Şablon klasörünüzün yolu
+        'DIRS': [os.path.join(BASE_DIR, 'elmirin/template')],  # Şablon klasörünüzün yolu
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from home import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
 ]
